2f2ee5cfcadd2b23f038adce81637024a5dbb88e
flashbotshere <94281579+flashbotshere@users.noreply.github.com>
2021-11-14 16:58:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/flashbotshere/flashautofilterer.git /flashautofilterer
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
 fi
-cd /EvaMaria
+cd /flashautofilterer
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
